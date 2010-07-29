aa2da98d4ab74d0710dd61015d81ae49c5437dc1
Christian Heilmann <chris.heilmann@gmail.com>
2010-07-29 08:01:16 -0700

    removed keys, oops:)

--- yql-with-php-authenticated/keys.php
+++ yql-with-php-authenticated/keys.php
@@ -1,8 +1,6 @@
 <?php
 // Add your consumer key and secret here - you can get it at 
-// developer.yahoo.com
-define("CONSUMER_KEY","dj0yJmk9d01oR1NuTWFYYzQ4JmQ9WVdrOVpHSkVTWGt5Tm1VbWNHbzlPVGc1TnpjM05EWXkmcz1jb25zdW1lcnNlY3JldCZ4PTMw");
-define("CONSUMER_SECRET","ff10ede410bb9f226780d3452663769cc4594755");   
-// define("CONSUMER_KEY","");
-// define("CONSUMER_SECRET","");   
+// developer.yahoo.com 
+define("CONSUMER_KEY","");
+define("CONSUMER_SECRET","");   
 ?>
